e5bb7519a07bf85faa58bfdef7b06c6d6be05eed
PARIMAL BISWAS <99968891+parimalbiswas@users.noreply.github.com>
2023-07-22 19:46:46 +0530

    Update and rename Parimal.java to Parimal.js

--- Parimal.java
+++
@@ -1,9 +0,0 @@
-public class Parimal {
-    public static void main(String[] args) {
-        System.out.println("Have a nice look - my_new_branch_TEST_ONLY");
-        System.out.println("SEcond Line");
-        System.out.println("Chnage in test 1++++++++++++++++");
-        System.out.println("Added 4th line from test");
-        System.out.println("Yes");
-    }
-}
